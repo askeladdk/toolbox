f3176c8ecd97d056ce03014fff8cebd54d13e90a
askeladdk <askeladdk@users.noreply.github.com>
2023-02-20 23:57:47 +0100

    Update test assertion helpers

--- internal/require/require.go
+++ internal/require/require.go
@@ -5,15 +5,22 @@ import (
 	"testing"
 )
 
-func Equal(t *testing.T, expected, got any, args ...any) {
+func Equal[T any](t testing.TB, expected, got T, args ...any) {
 	if !reflect.DeepEqual(expected, got) {
-		args = append([]any{expected, "!=", got}, args...)
+		t.Helper()
+		if len(args) == 0 {
+			t.Fatal(expected, "is not", got)
+		}
 		t.Fatal(args...)
 	}
 }
 
-func True(t *testing.T, test bool, args ...any) {
+func True(t testing.TB, test bool, args ...any) {
 	if !test {
+		t.Helper()
+		if len(args) == 0 {
+			t.Fatal("is false")
+		}
 		t.Fatal(args...)
 	}
 }
